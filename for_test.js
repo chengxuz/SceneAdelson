db22b7a2d08c204449960672df5f803eace37765
zcx_win8 <nbfhzcx002@163.com>
2016-11-17 00:23:45 -0800

    Add save pixel support

---
+++ for_test.js
@@ -0,0 +1,7 @@
+var j =0
+
+for (var i = 0; i < 3; i++) { 
+    j = j + 1
+}
+
+console.log(j)
---
+++ test_save_pixel.js
@@ -0,0 +1,13 @@
+var zeros = require("zeros")
+var savePixels = require("save-pixels")
+var fs = require("fs")
+
+var out = fs.createWriteStream('test_img_ge.png')
+
+//Create an image
+var x = zeros([32, 32])
+x.set(16, 16, 255)
+
+//Save to a file
+//savePixels(x, "png").pipe(process.stdout)
+savePixels(x, "png").pipe(out)
