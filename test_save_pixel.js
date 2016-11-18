f2c6e818fbc3df2c9362d18462ab7def95ce9a87
zcx_win8 <nbfhzcx002@163.com>
2016-11-18 00:53:41 -0800

    Batch mode added

--- test_save_pixel.js
+++ test_save_pixel.js
@@ -5,9 +5,10 @@ var fs = require("fs")
 var out = fs.createWriteStream('test_img_ge.png')
 
 //Create an image
-var x = zeros([32, 32])
-x.set(16, 16, 255)
+//var x = zeros([32, 32])
+//x.set(16, 16, 255)
 
 //Save to a file
 //savePixels(x, "png").pipe(process.stdout)
-savePixels(x, "png").pipe(out)
+//savePixels(x, "png").pipe(out)
+console.log(parseFloat('101', 10) + 10)
